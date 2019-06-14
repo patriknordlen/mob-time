a575b982c22978fb806baa931d2214c2a03ae6c0
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-06-14 14:33:32 +0200

    Add a basic index

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!\n');
+});
+
+server.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${PORT}/`);
+});
