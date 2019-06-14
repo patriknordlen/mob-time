c299371b640e56a56aab23141134827042ac4469
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-06-14 15:57:53 +0200

    Make mocha test actually fail

--- test/spec.js
+++ test/spec.js
@@ -4,22 +4,25 @@ var timer = require("../current-timer");
 
 describe("Mob Server", function() {
     describe("index", function() {
-        it("returns 200", function(){
+        it("returns 200", function(done){
             request.get("http://localhost:3000/", function(error, response, body){
                 expect(response.statusCode).to.equal(200);
+                done();
             });
         });
     });
 
     describe("start", function() {
-        it("returns 200", function(){
-            request.post("http://localhost:3000/start?time-left=12", function(error, response, body){
+        it("returns 200", function(done){
+            request.post("http://localhost:3000/start?timeLeft=12", function(error, response, body){
                 expect(response.statusCode).to.equal(200);
+                done();
             });
         });
-        it("starts a timer of the given amount", function() {
-            request.post("http://localhost:3000/start?time-left=12", function(error, response, body){
-                expect(timer.timeLeft).to.equal(12 * 60);
+        it("starts a timer of the given amount", function(done) {
+            request.post("http://localhost:3000/start?timeLeft=14", function(error, response, body){
+                expect(timer.timeLeft).to.equal(14 * 60);
+                done();
             });
         });
     });
