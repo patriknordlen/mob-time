2ba36f99578a8827e22e0a2f56bb92536c8d226e
HadrienMP <public@hadrienmp.fr>
2019-06-14 21:55:14 +0200

    Return a json for timeleft to accomodate many uses

--- index.js
+++ index.js
@@ -2,36 +2,39 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-var mobTimer = {
-    periodInSeconds: 0,
-    elapsedMs: 0,
-    startTime: 0,
-    currentTimer: 0,
-
-    initTimer: function(periodInMinutes) {
-        this.periodInSeconds = parseInt(periodInMinutes) * 60;
+class MobTurn {
+    constructor(lengthInMinutes) {
+        this.lengthInSeconds = lengthInMinutes * 60;
+        this.elapsedMs = 0;
         this.startTime = new Date();
-
-        if (this.currentTimer) {
-            clearInterval(this.currentTimer);
-        }
-
-        var self = this;
-        this.currentTimer = setInterval(function () {
+        let self = this;
+        this.timer = setInterval(function () {
             self.elapsedMs = new Date() - self.startTime;
         }, 100);
     }
-};
+
+    timeLeftInMillis() {
+        return this.lengthInSeconds * 1000 - this.elapsedMs;
+    }
+}
+
+let currentTurn = undefined;
 
 app.get("/", function (req, res) {
     res.send("Mob Time Server !");
 });
 app.post("/start", function (req, res) {
-    mobTimer.initTimer(req.query.timeLeft);
+    currentTurn = new MobTurn(parseInt(req.query.lengthInMinutes));
     res.sendStatus(200);
 });
 app.get("/timeLeft", function (req, res) {
-   res.send((mobTimer.periodInSeconds * 1000 - mobTimer.elapsedMs) + "");
+    let millis = currentTurn.timeLeftInMillis();
+    let seconds = millis / 1000;
+    res.send(JSON.stringify({
+        millis: millis,
+        minutes: Math.floor(seconds / 60),
+        seconds: Math.floor(seconds % 60),
+    }));
 });
 
 app.listen(PORT, function () {
