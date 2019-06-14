36301798e9fbe8a8fe00a8a46770e3d049e607bd
srezzouqi <srezzouqi@financeactive.com>
2019-06-14 14:40:07 +0200

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
+    res.end("Ceci n'est pas un test!\n");
 });
 
 server.listen(PORT, hostname, () => {
