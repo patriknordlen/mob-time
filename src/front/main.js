2653c8e9a939d43c6fc5da9c11ffa4d4770489ba
HadrienMP <public@hadrienmp.fr>
2019-06-18 19:38:58 +0200

    Fix javascript smells

--- src/front/main.js
+++ src/front/main.js
@@ -64,7 +64,7 @@ function toHumanReadableString(time) {
 function updateTimeLeftAsync() {
 	let xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
+		if (this.readyState === 4 && this.status === 200) {
 			timeLeftResponse = JSON.parse(this.responseText);
 			display(timeLeftResponse);
 		}
