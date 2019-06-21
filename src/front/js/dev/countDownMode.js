1c6a341e9ede55692239b9c0403ca6903dbae8c1
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-06-21 13:44:29 +0200

    Improve style

--- src/front/js/dev/countDownMode.js
+++ src/front/js/dev/countDownMode.js
@@ -4,11 +4,9 @@ export function turnOn() {
     const container = document.getElementById("container");
     container.classList.remove("counting");
     container.classList.add("counting");
-    document.getElementsByTagName("h1")[0].innerText = "En cours";
 }
 
 export function turnOff() {
     const container = document.getElementById("container");
     container.classList.remove("counting");
-    document.getElementsByTagName("h1")[0].innerText = display.appTitle;
 }
