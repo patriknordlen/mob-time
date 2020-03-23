2b0d27a099fe51b8157e2e2b5361eadeb1fa8dbd
Thomas Carpaye <tcarpaye@financeactive.com>
2020-03-23 10:58:15 +0100

    Change storage key : add suffix -turn

--- src/back/mob_turns.js
+++ src/back/mob_turns.js
@@ -2,13 +2,18 @@ const MobTurn = require("./MobTurn");
 const store = process.env.REDIS_URL === undefined ? require("./storages/local_storage") : require("./storages/redis");
 
 exports.get = async name => {
-    let json = await store.get(name);
+    let json = await store.get(toTurnName(name));
     if (json == null) return new MobTurn();
     let data = JSON.parse(json);
     return new MobTurn(data.lengthInSeconds, new Date(data.startTime));
 };
+
 exports.start = (name, lengthInMinutes) => {
     let mobTurn = new MobTurn(lengthInMinutes * 60);
-    store.save(name, JSON.stringify(mobTurn));
+    store.save(toTurnName(name), JSON.stringify(mobTurn));
 };
-exports.stop = name => store.delete(name);
+
+function toTurnName(name) {
+    return name + "-turn";
+}
+exports.stop = name => store.delete(toTurnName(name));
