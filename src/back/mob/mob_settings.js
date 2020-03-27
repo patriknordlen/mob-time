22058f4ac0d8302ddd010f8c11e8162bd559e7d8
hadrienmp <public@hadrienmp.fr>
2020-03-27 08:19:14 +0200

    Change the settings format to enable field addition

--- src/back/mob/mob_settings.js
+++ src/back/mob/mob_settings.js
@@ -1,11 +1,22 @@
 const store = require("../stores/stores").get();
 
 exports.saveLength = function(name, lengthInMinutes) {
-    store.save(toSettingsName(name), lengthInMinutes);
+    let settings = {
+        formatVersion: 1,
+        lengthInMinutes: lengthInMinutes
+    };
+    store.save(toSettingsName(name), JSON.stringify(settings));
 };
 
 exports.getLength = async function (name) {
-    return await store.get(toSettingsName(name)) || 10;
+    let rawSettings = await store.get(toSettingsName(name));
+    if (!rawSettings) return 10;
+    if (!rawSettings.startsWith("{")) {
+        await this.saveLength(name, rawSettings);
+        return this.getLength(name);
+    }
+    let settings = JSON.parse(rawSettings);
+    return settings.lengthInMinutes
 };
 
 function toSettingsName(name) {
