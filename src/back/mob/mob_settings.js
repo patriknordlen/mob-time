1610717c5a9d3d46a1e7a3a530d6b754ff8bdd9a
hadrienmp <public@hadrienmp.fr>
2020-03-27 08:59:01 +0200

    Change the settings format to enable field addition

--- src/back/mob/mob_settings.js
+++ src/back/mob/mob_settings.js
@@ -1,23 +1,39 @@
 const store = require("../stores/stores").get();
 
-exports.saveLength = function(name, lengthInMinutes) {
+const defaultSettings = {
+    formatVersion: 1,
+    lengthInMinutes: 10
+};
+
+exports.saveLength = async function(name, lengthInMinutes) {
     let settings = {
         formatVersion: 1,
         lengthInMinutes: lengthInMinutes
     };
-    store.save(toSettingsName(name), JSON.stringify(settings));
+    await this.save(name, settings);
+};
+
+exports.save = async function(name, settings) {
+    await store.save(toSettingsName(name), JSON.stringify(settings));
 };
 
 exports.getLength = async function (name) {
-    let rawSettings = await store.get(toSettingsName(name));
-    if (!rawSettings) return 10;
-    if (!rawSettings.startsWith("{")) {
-        await this.saveLength(name, rawSettings);
-        return this.getLength(name);
-    }
-    let settings = JSON.parse(rawSettings);
-    return settings.lengthInMinutes
+    return this.get(name).then(value => value.lengthInMinutes);
 };
+
+exports.get = async function (name) {
+    let rawSettings = await store.get(toSettingsName(name));
+    if (!rawSettings) return defaultSettings;
+    if (isRawLength(rawSettings)) {
+        await this.saveLength(name, rawSettings);
+        return this.get(name);
+    }
+    return  JSON.parse(rawSettings);
+};
+
+function isRawLength(rawSettings) {
+    return !rawSettings.startsWith("{");
+}
 
 function toSettingsName(name) {
     return name + "-settings";
