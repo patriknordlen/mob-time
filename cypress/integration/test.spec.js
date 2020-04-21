539d813b24e426b6fb6b418d693d03df95c32e7d
hadrienmp <public@hadrienmp.fr>
2020-04-21 22:31:18 +0200

    [#2] Reproduce with cypress

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,12 @@
+describe("Stuff", () => {
+    it("does stuff", () => {
+        cy.visit("http://localhost:3000/");
+        cy.get("#mob-name").type("with spaces");
+        cy.get("#submit").click();
+        cy.get("#start-pause").click();
+        cy.get("#container").should('have.class', 'counting');
+        cy.get("#time-left")
+          .should('have.css', 'display', 'inline-block')
+          .and('not.contain.text', '0 s');
+    })
+})
