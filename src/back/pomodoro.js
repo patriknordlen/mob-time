26870fbeda116ff6ff62268471cfccf3f6c1fb3c
hadrienmp <public@hadrienmp.fr>
2020-05-03 18:11:48 +0200

    Implement a turnStarted function in pomodoro.js

--- src/back/pomodoro.js
+++ src/back/pomodoro.js
@@ -1,3 +1,6 @@
+const store = require("./stores/stores").get();
+const allSettings = require("./mob/settings");
+
 exports.get = function () {
     const features = process.env.FEATURES || "";
     if (features.includes("pomodoro")) return new Pomodoro();
@@ -8,10 +11,29 @@ class Pomodoro {
     status() {
         return {};
     }
+
+    async turnStarted(name, start) {
+        let settings = await allSettings.get(name);
+
+        if (!settings.pomodoro.active) return;
+
+        let pomodoroName = `${name}-pomodoro`;
+        let currentPomodoro = await store.get(pomodoroName);
+        if (currentPomodoro) return;
+
+        store.save(pomodoroName, JSON.stringify({
+            formatVersion: 1,
+            start: start,
+            length: settings.lengthInMinutes * settings.pomodoro.turns
+        }));
+    }
 }
 
 class Off {
     status() {
         return null;
+    }
+
+    turnStarted() {
     }
 }
