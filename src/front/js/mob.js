14dcee420e45a87eaacea30e7dfd7344c3d8ad93
Patrik Nordlén <patriknordlen@spotify.com>
2020-11-03 09:15:26 +0100

    Remove amplitude stuff

--- src/front/js/mob.js
+++ src/front/js/mob.js
@@ -1,7 +1,6 @@
 const sound = require("./sound");
 const display = require("./display/display");
 require("./display/countDownMode");
-const amplitude = require("./amplitude,").get();
 const mobTimer = require("./spi/mobTimer");
 const eventsModule = require("./events");
 const settings = require("./settings");
@@ -30,17 +29,14 @@ display.init();
 document.forms.container.onsubmit = function (event) {
     event.preventDefault();
     if (turn.isInProgress()) {
-        amplitude.getInstance().logEvent('STOP_MOB');
         socket.emit("interrupt mob", mobName);
         return;
     }
     if (sound.isPlaying()) {
         sound.stop();
-        amplitude.getInstance().logEvent('STOP_SOUND');
         return;
     }
     socket.emit("start mob", mobName, settings.membersAsArray(), settings.minutesByPerson());
-    // amplitude.getInstance().logEvent('START_MOB');
 };
 
 new ClipboardJS("#share-room", {
