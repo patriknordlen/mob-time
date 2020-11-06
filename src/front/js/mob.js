143d86f262bb8e0418a0d73f0a6ce63deecff0c1
Patrik Nordlén <patriknordlen@spotify.com>
2020-11-06 22:36:54 +0100

    Improve notifications handling

--- src/front/js/mob.js
+++ src/front/js/mob.js
@@ -6,13 +6,14 @@ const eventsModule = require("./events");
 const settings = require("./settings");
 const turn = require("./mob/turn");
 const pomodoro = require("./pomodoro/countdown");
+const enableNotifications = document.getElementById("enableNotifications");
 
 mobTimer.timeLeftIn(mobName, update);
 setInterval(() => mobTimer.timeLeftIn(mobName, update), 500);
 
 function update(timerStatus) {
-    eventsModule.throwEventFor(timerStatus, turn.isInProgress());
-    display.displayTimeLeft(timerStatus);
+  eventsModule.throwEventFor(timerStatus, turn.isInProgress());
+  display.displayTimeLeft(timerStatus);
 }
 
 // --------------------------------------------
@@ -27,37 +28,52 @@ socket.emit("join", mobName);
 sound.init();
 display.init();
 document.forms.container.onsubmit = function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (Notification.permission != 'granted') {
-        Notification.requestPermission();
-    }
-    
-    if (turn.isInProgress()) {
-        socket.emit("interrupt mob", mobName);
-        return;
-    }
-    if (sound.isPlaying()) {
-        sound.stop();
-        return;
-    }
-    socket.emit("start mob", mobName, settings.membersAsArray(), settings.minutesByPerson());
+  if (Notification.permission != "granted") {
+    Notification.requestPermission();
+  }
+
+  if (turn.isInProgress()) {
+    socket.emit("interrupt mob", mobName);
+    return;
+  }
+  if (sound.isPlaying()) {
+    sound.stop();
+    return;
+  }
+  socket.emit("start mob", mobName, settings.membersAsArray(), settings.minutesByPerson());
 };
 
 new ClipboardJS("#share-room", {
-    text: () => window.location.href
-}).on('success', () => {
-    alert('A link to this mob has been copied in your clipboard');
+  text: () => window.location.href,
+}).on("success", () => {
+  alert("A link to this mob has been copied in your clipboard");
 });
 
 pomodoro.setup();
 require("./pomodoro/settings").setup(socket, mobName);
 settings.setupSync(socket, mobName);
 
-socket.on("start mob", function(member) {
-    if (Notification.permission != 'granted') {
-        Notification.requestPermission();
-    }
-
-    var notify = new Notification("Turn started", { body: member + " started a turn."});
+socket.on("start mob", function (member) {
+  var notify = new Notification("Turn started", { body: member + " started a turn." });
 });
+
+window.addEventListener(
+  "DOMContentLoaded",
+  (event) => {
+    if (Notification.permission != "granted") {
+      var enableNotificationsButton = document.createElement("button");
+      enableNotificationsButton.id = "enableNotificationsButton";
+      enableNotificationsButton.innerText = "Enable notifications";
+      enableNotificationsButton.onclick = (event) => {
+        event.preventDefault();
+        Notification.requestPermission().then(() => {
+            // enableNotificationsButton.remove();
+            document.getElementById("enableNotifications").innerHTML = "Notifications enabled!";
+      })};
+      document.getElementById("enableNotifications").appendChild(enableNotificationsButton);
+    }
+  },
+  false
+);
