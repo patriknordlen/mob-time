9582197aa17a42273ca775a380da96e5e43e70b3
Patrik Nordlén <patrik.nordlen@einride.tech>
2023-08-24 21:16:37 +0200

    dummy commit 3

--- config.rb
+++ config.rb
@@ -3,6 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
+
 css_dir = "public/css"
 sass_dir = "src/front/sass"
 images_dir = "public/images"
